797e8e37afa40213d8944894e4b6e740ab03aa37
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-26 09:49:29 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development'
 });
 
+const nextConfig = {
+  reactStrictMode: true,
   webpack: (config) => {
     return config;
   },
